69dd63e6839b275bbe2250a49985e6eee6d194d8
Mikael Göransson <github@mgor.se>
2022-02-09 19:37:31 +0100

    add header identifying restapi user

--- grizzly/users/restapi.py
+++ grizzly/users/restapi.py
@@ -154,6 +154,7 @@ class RestApiUser(ResponseHandler, RequestLogger, ContextVariables, HttpRequests
         self.headers = {
             'Authorization': None,
             'Content-Type': 'application/json',
+            'x-grizzly-user': f'{self.__class__.__name__}-{id(self)}',
         }
 
         self.session_started = None
