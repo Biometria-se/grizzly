5403464f5e216be65d82e81e6971b401c6293b36
Mikael Göransson <github@mgor.se>
2024-03-08 18:12:36 +0100

    mypy complains about redundant cast
    it was necessary before, since it couldn't infer the correct type otherwise (type[T]), but when removed, it complains that the type is `type[T]` and hence doesn't have some attributes that the type actually has...

--- grizzly/utils.py
+++ grizzly/utils.py
@@ -86,7 +86,7 @@ def create_user_class_type(scenario: GrizzlyContextScenario, global_context: Opt
         message = f'scenario {scenario.description} does not have a user type set'
         raise ValueError(message)
 
-    base_user_class_type = cast(Type['GrizzlyUser'], ModuleLoader['GrizzlyUser'].load('grizzly.users', scenario.user.class_name))
+    base_user_class_type = cast(Type['GrizzlyUser'], ModuleLoader['GrizzlyUser'].load('grizzly.users', scenario.user.class_name))  # type: ignore[redundant-cast]
     user_class_name = f'{scenario.user.class_name}_{scenario.identifier}'
 
     context: Dict[str, Any] = {}
