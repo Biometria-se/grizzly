faad0f0dbbaf2b0977a1371b6be81ec5b58c7a3f
Mikael Göransson <github@mgor.se>
2024-06-05 14:34:54 +0200

    merge environment configuration files

--- docs/content/framework/usage/variables/environment-configuration.md
+++ docs/content/framework/usage/variables/environment-configuration.md
@@ -8,24 +8,6 @@ The environment configuration file can also be used to store credentials and oth
 
 Internally `grizzly` will check if the environment variable `GRIZZLY_CONFIGURATION_FILE` is set and contains a valid environment configuration file. When using `grizzly-cli` you specify the file with `-e/--environment-file` which then will be set as a value for `GRIZZLY_CONFIGURATION_FILE`.
 
-## Format
-
-An example environment configuration file:
-
-```yaml
-configuration:
-    frontend:
-        host: https://www.example.com
-    backend:
-        host: https://backend.example.com
-        auth:
-            user:
-                username: bob
-                password: Who-the-f-is-alice
-```
-
-The only rule for any nodes under `configuration` is that it **must** be a dictionary, since the path to a value will be flattened.
-
 ## Usage
 
 In a feature file the dictionary can then be used by prefixing the path of a node under `configuration` with `$conf::<tree path to variable>$`.
@@ -50,3 +32,67 @@ Feature: application test
 ```
 
 This feature can now be run against a different environment just by creating a new environment configuration file with different values.
+
+## Basic format
+
+An example basic environment configuration file:
+
+```yaml
+configuration:
+    frontend:
+        host: https://www.example.com
+    backend:
+        host: https://backend.example.com
+        auth:
+            user:
+                username: bob
+                password: Who-the-f-is-alice
+```
+
+The only rule for any nodes under `configuration` is that it **must** be a dictionary, since the path to a value will be flattened.
+
+## Advanced format
+
+If you plan to run in multiple environments there could be cases that some environment variables are the same in all of the environments. If this is the case, it is possible
+to merge multiple environment configuration files (`{% merge "<file1>" ["file2", ... ["fileN"]]}`), so that common values are only stored in one place.
+
+The merging will take place from the bottom up, so conents in the source file where the `{% merge ... %}` statement is found, any duplicate keys in `fileN` will override the source values, `file2` will override those, and lastly `file1` will override those. So the first specified file to the `{% merge ... %}` statement will have highest precedence.
+
+```
+source < fileN < ... < file2 < file1
+```
+
+Example:
+
+```yaml title="./common.yaml"
+configuration:
+    backend:
+        auth:
+            user:
+                username: bob
+                password: Who-the-f-is-alice
+```
+
+```yaml title="./staging.yaml"
+{% merge "./common.yaml" %}
+configuration:
+    frontend:
+        host: https://www.staging.example.com
+    backend:
+        host: https://backend.staging.example.com
+```
+
+```yaml title="./test.yaml"
+{% merge "./common.yaml" %}
+configuration:
+    frontend:
+        host: https://www.test.example.com
+    backend:
+        host: https://backend.test.example.com
+```
+
+For both environment files, this will produce the following configuration keys:
+- `frontend.host`
+- `backend.host`
+- `backend.auth.user.username`
+- `backend.auth.user.password`
--- grizzly/utils.py
+++ grizzly/utils.py
@@ -12,11 +12,13 @@ from json import dumps as jsondumps
 from json import loads as jsonloads
 from os import environ
 from pathlib import Path
-from typing import TYPE_CHECKING, Any, Dict, Generator, Generic, List, Optional, Tuple, Type, Union, cast
+from typing import TYPE_CHECKING, Any, ClassVar, Dict, Generator, Generic, Iterable, List, Optional, Set, Tuple, Type, Union, cast
 from unicodedata import normalize as __normalize
 
 from dateutil.parser import ParserError
 from dateutil.parser import parse as dateparser
+from jinja2.lexer import Token, TokenStream
+from jinja2_simple_tags import StandaloneTag
 from locust.stats import STATS_NAME_WIDTH
 
 from grizzly.types import T
@@ -53,6 +55,96 @@ class ModuleLoader(Generic[T]):
         class_type_instance = globals()[class_name]
 
         return cast(Type[T], class_type_instance)
+
+class MergeYamlTag(StandaloneTag):
+    tags: ClassVar[Set[str]] = {'merge'}
+
+    def preprocess(
+        self, source: str, name: Optional[str], filename: Optional[str] = None,
+    ) -> str:
+        self._source = source
+        return cast(str, super().preprocess(source, name, filename))
+
+    def render(self, filename: str, *filenames: str) -> str:
+        buffer: List[str] = []
+
+        files = [filename, *filenames]
+
+        for file in files:
+            merge_file = Path(file)
+
+            # check if relative to parent feature file
+            if not merge_file.exists():
+                merge_file = (self.environment.source_file.parent / merge_file).resolve()
+
+            if not merge_file.exists():
+                raise FileNotFoundError(merge_file)
+
+            merge_content = merge_file.read_text()
+
+            if merge_content[0:3] != '---':
+                buffer.append('---')
+
+            buffer.append(merge_content)
+
+        if self._source[0:3] != '---':
+            buffer.append('---')
+
+        return '\n'.join(buffer)
+
+    def filter_stream(self, stream: TokenStream) -> Union[TokenStream, Iterable[Token]]:
+        """Everything outside of `{% merge ... %}` should be treated as "data", e.g. plain text."""
+        in_merge = False
+        in_variable = False
+        in_block_comment = False
+
+        variable_begin_pos = -1
+        variable_end_pos = 0
+        block_begin_pos = -1
+        block_end_pos = 0
+        source_lines = self._source.splitlines()
+
+        for token in stream:
+            if token.type == 'block_begin' and stream.current.value in self.tags:
+                in_merge = True
+                current_line = source_lines[token.lineno - 1].lstrip()
+                in_block_comment = current_line.startswith('#')
+                block_begin_pos = self._source.index(token.value, block_begin_pos + 1)
+
+            if not in_merge:
+                if token.type == 'variable_end':
+                    # Find variable end in the source
+                    variable_end_pos = self._source.index(token.value, variable_begin_pos)
+                    # Extract the variable definition substring and use as token value
+                    token_value = self._source[variable_begin_pos:variable_end_pos + len(token.value)]
+                    in_variable = False
+                elif token.type == 'variable_begin':
+                    # Find variable start in the source
+                    variable_begin_pos = self._source.index(token.value, variable_begin_pos + 1)
+                    in_variable = True
+                else:
+                    token_value = token.value
+
+                if in_variable:
+                    # While handling in-variable tokens, withhold values until
+                    # the end of the variable is reached
+                    continue
+
+                filtered_token = Token(token.lineno, 'data', token_value)
+            elif token.type == 'block_end' and in_block_comment:
+                in_block_comment = False
+                block_end_pos = self._source.index(token.value, block_begin_pos)
+                token_value = self._source[block_begin_pos:block_end_pos + len(token.value)]
+                filtered_token = Token(token.lineno, 'data', token_value)
+            elif in_block_comment:
+                continue
+            else:
+                filtered_token = token
+
+            yield filtered_token
+
+            if in_merge and token.type == 'block_end':
+                in_merge = False
 
 
 @contextmanager
