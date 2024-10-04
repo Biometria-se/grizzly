a8a86c73f87e14fd024ee21110dc35e404841c2a
Mikael Göransson <github@mgor.se>
2024-10-04 07:13:00 +0200

    fixed unit tests

--- tests/unit/test_grizzly/steps/test_utils.py
+++ tests/unit/test_grizzly/steps/test_utils.py
@@ -21,10 +21,31 @@ def test_step_utils_fail(behave_fixture: BehaveFixture) -> None:
 def test_step_utils_add_orphan_template(behave_fixture: BehaveFixture) -> None:
     behave = behave_fixture.context
     grizzly = behave_fixture.grizzly
-    grizzly.scenarios.create(behave_fixture.create_scenario('test'))
+
+    test_scenario = grizzly.scenarios.create(behave_fixture.create_scenario('test'))
+    grizzly.scenarios.create(behave_fixture.create_scenario('second'))
+    grizzly.scenarios.create(behave_fixture.create_scenario('third'))
+    grizzly.scenarios.select(test_scenario.behave)
+
+    behave_fixture.create_step('test step', in_background=False, context=behave)
 
     assert grizzly.scenario.orphan_templates == []
 
     step_utils_add_orphan_template(behave, '{{ hello world }}')
 
     assert grizzly.scenario.orphan_templates == ['{{ hello world }}']
+
+    for scenario in grizzly.scenarios:
+        if scenario is test_scenario:
+            continue
+
+        assert scenario.orphan_templates == []
+
+    test_scenario.orphan_templates.clear()
+
+    behave_fixture.create_step('test step background', in_background=True, context=behave)
+
+    step_utils_add_orphan_template(behave, '{{ foobar }}')
+
+    for scenario in grizzly.scenarios:
+        assert scenario.orphan_templates == ['{{ foobar }}']
