317482b2c8247c90cc064053a9550ee45243662f
Mikael Göransson <github@mgor.se>
2024-11-27 13:48:24 +0100

    grizzly specific configuration of locust (and it's own) loggers.

--- grizzly/locust.py
+++ grizzly/locust.py
@@ -21,7 +21,6 @@ import gevent.event
 from locust import events
 from locust import stats as lstats
 from locust.dispatch import UsersDispatcher
-from locust.log import setup_logging
 from locust.user.users import User
 from locust.util.timespan import parse_timespan
 from roundrobin import smooth
@@ -29,6 +28,7 @@ from roundrobin import smooth
 from . import __locust_version__, __version__
 from .context import GrizzlyContext
 from .listeners import init, init_statistics_listener, locust_test_start, locust_test_stop, spawning_complete, validate_result, worker_report
+from .log import setup_logging
 from .testdata.utils import initialize_testdata
 from .testdata.variables.csv_writer import open_files
 from .types import RequestType, TestdataType
